438a0109dd64759e700abcf8d63e20597b2f915d
Bjorn Van Acker <bjorn@sumocoders.be>
2016-06-06 16:19:15 +0200

    Add tests for DeleteUserHandler

---
+++ Tests/Command/DeleteUserHandlerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SumoCoders\FrameworkMultiUserBundle\Tests\Command;
+
+use SumoCoders\FrameworkMultiUserBundle\Command\DeleteUser;
+use SumoCoders\FrameworkMultiUserBundle\Command\DeleteUserHandler;
+use SumoCoders\FrameworkMultiUserBundle\User\InMemoryUserRepository;
+
+class DeleteUserHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    private $userRepository;
+
+    public function setUp()
+    {
+        $this->userRepository = new InMemoryUserRepository();
+
+    }
+
+    /**
+     * Test if DeleteUserHandler gets handled.
+     */
+    public function testUpdateUserGetsHandled()
+    {
+        $handler = new DeleteUserHandler($this->userRepository);
+
+        $command = new DeleteUser();
+
+        $deletingUser = $this->userRepository->findByUsername('wouter');
+
+        $command->user = $deletingUser;
+
+        $handler->handle($command);
+
+        $this->assertNotNull($deletingUser);
+        $this->assertNull(
+            $this->userRepository->findByUsername('wouter')
+        );
+    }
+}
