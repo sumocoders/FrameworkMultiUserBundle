abfd3d71e007f6ba7ea5d91b71d47f100f17fcba
Bjorn Van Acker <bjorn@sumocoders.be>
2016-06-06 16:03:07 +0200

    Add tests for UpdateUserHandler

---
+++ Tests/Command/UpdateUserHandlerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SumoCoders\FrameworkMultiUserBundle\Tests\Command;
+
+use SumoCoders\FrameworkMultiUserBundle\Command\UpdateUser;
+use SumoCoders\FrameworkMultiUserBundle\Command\UpdateUserHandler;
+use SumoCoders\FrameworkMultiUserBundle\User\InMemoryUserRepository;
+
+class UpdateUserHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    private $userRepository;
+
+    public function setUp()
+    {
+        $this->userRepository = new InMemoryUserRepository();
+
+    }
+
+    /**
+     * Test if UpdateUserHandler gets handled.
+     */
+    public function testUpdateUserGetsHandled()
+    {
+        $handler = new UpdateUserHandler($this->userRepository);
+
+        $command = new UpdateUser();
+        $command->username = 'wouter';
+        $command->displayName = 'sumocoders';
+        $command->password = 'randomPassword';
+
+        $updatingUser = $this->userRepository->findByUsername('wouter');
+
+        $command->user = $updatingUser;
+
+        $handler->handle($command);
+
+        $this->assertEquals(
+            'wouter',
+            $this->userRepository->findByUsername('wouter')->getUsername()
+        );
+        $this->assertEquals(
+            'sumocoders',
+            $this->userRepository->findByUsername('wouter')->getDisplayName()
+        );
+        $this->assertEquals(
+            'randomPassword',
+            $this->userRepository->findByUsername('wouter')->getPassword()
+        );
+        $this->assertNotEquals(
+            $updatingUser->getDisplayName(),
+            $this->userRepository->findByUsername('wouter')->getDisplayName()
+        );
+        $this->assertNotEquals(
+            $updatingUser->getPassword(),
+            $this->userRepository->findByUsername('wouter')->getPassword()
+        );
+    }
+}
