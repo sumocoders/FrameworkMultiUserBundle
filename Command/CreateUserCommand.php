2b086914d8bfca4b9a78e8c2a136ea961f4f7cc2
Bjorn Van Acker <bjorn@sumocoders.be>
2016-06-07 13:30:21 +0200

    Add CLI command for CreateUser

---
+++ Command/CreateUserCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SumoCoders\FrameworkMultiUserBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class CreateUserCommand.
+ */
+class CreateUserCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('sumocoders_multiuser:create')
+            ->setDescription('Create a user entity')
+            ->addArgument(
+                'username',
+                InputArgument::REQUIRED,
+                'The username of the user'
+            )
+            ->addArgument(
+                'password',
+                InputArgument::REQUIRED,
+                'The password for the user'
+            )
+            ->addArgument(
+                'displayName',
+                InputArgument::REQUIRED,
+                'The display name for the user'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $repository = $this->getContainer()->get('multi_user.user.repository');
+
+        $handler = new CreateUserHandler($repository);
+
+        $username = $input->getArgument('username');
+        $password = $input->getArgument('password');
+        $displayName = $input->getArgument('displayName');
+
+        $command = new CreateUser();
+        $command->username = $username;
+        $command->password = $password;
+        $command->displayName = $displayName;
+
+        $handler->handle($command);
+
+        $output->writeln($username . ' has been created');
+    }
+}
