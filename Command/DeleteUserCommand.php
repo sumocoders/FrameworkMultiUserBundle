725f6fcbf7a7fc0d362e8b156262e0a877b3b06b
Bjorn Van Acker <bjorn@sumocoders.be>
2016-06-07 13:39:49 +0200

    Add CLI command for DeleteUser

---
+++ Command/DeleteUserCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SumoCoders\FrameworkMultiUserBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class DeleteUserCommand.
+ */
+class DeleteUserCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('sumocoders_multiuser:delete')
+            ->setDescription('Delete a user entity')
+            ->addArgument(
+                'username',
+                InputArgument::REQUIRED,
+                'The username of the user'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $repository = $this->getContainer()->get('multi_user.user.repository');
+
+        $handler = new DeleteUserHandler($repository);
+
+        $username = $input->getArgument('username');
+        $user = $repository->findByUsername($username);
+
+        if (!$user) {
+            $output->writeln('<error>'.$username.' doesn\'t exists');
+            exit;
+        }
+
+        $command = new DeleteUser();
+        $command->user = $user;
+
+        $handler->handle($command);
+
+        $output->writeln($username . ' has been deleted');
+    }
+}
