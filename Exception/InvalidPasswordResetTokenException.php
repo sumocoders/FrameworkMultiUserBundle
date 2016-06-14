5e4dd16724db556878e770b7c520850ae2135117
bjorvack <bjorvack@gmail.com>
2016-06-14 09:21:35 +0200

    Create password reset token Add InvalidPasswordResetTokenException

---
+++ Exception/InvalidPasswordResetTokenException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SumoCoders\FrameworkMultiUserBundle\Exception;
+
+use Exception;
+
+class InvalidPasswordResetTokenException extends Exception
+{
+}
---
+++ Security/PasswordResetToken.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SumoCoders\FrameworkMultiUserBundle\Security;
+
+use SumoCoders\FrameworkMultiUserBundle\Exception\InvalidPasswordResetTokenException;
+use SumoCoders\FrameworkMultiUserBundle\User\UserInterface;
+
+class PasswordResetToken
+{
+    private function generatePasswordResetToken(){
+        return time() . base64_encode(random_bytes(10));
+    }
+
+    public static function validateToken(UserInterface $user, $token){
+        if($user->getPasswordResetToken() === $token){
+            return true;
+        }
+
+        throw new InvalidPasswordResetTokenException('The given token is not valid.');
+    }
+
+    public static function getToken(){
+        return self::generatePasswordResetToken();
+    }
+}
