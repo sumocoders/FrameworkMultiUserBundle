fb849c2998b8d964bd38d23b806bd9741ef674c6
stijnvrolijk <stijn@sumocoders.be>
2016-10-27 14:16:12 +0200

    Display better error messages when something goes wrong in the login

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -4,8 +4,13 @@ namespace SumoCoders\FrameworkMultiUserBundle\Controller;
 
 use SumoCoders\FrameworkMultiUserBundle\Security\FormAuthenticator;
 use SumoCoders\FrameworkMultiUserBundle\User\Interfaces\User;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -30,6 +35,9 @@ final class LoginController
      */
     private $tokenStorage;
 
+    /** @var Translator */
+    private $translator;
+
     /**
      * @param EngineInterface $templating
      * @param AuthenticationUtils $authenticationUtils
@@ -40,18 +48,17 @@ final class LoginController
         EngineInterface $templating,
         AuthenticationUtils $authenticationUtils,
         FormAuthenticator $formAuthenticator,
-        TokenStorage $tokenStorage
+        TokenStorage $tokenStorage,
+        Translator $translator
     ) {
         $this->templating = $templating;
         $this->authenticationUtils = $authenticationUtils;
         $this->formAuthenticator = $formAuthenticator;
         $this->tokenStorage = $tokenStorage;
+        $this->translator = $translator;
     }
 
-    /**
-     * @param Request $request
-     */
-    public function loginAction(Request $request)
+    public function loginAction()
     {
         if ($this->tokenStorage->getToken()->getUser() instanceof User) {
             return new RedirectResponse(
@@ -64,8 +71,45 @@ final class LoginController
         return $this->templating->renderResponse(
             'SumoCodersFrameworkMultiUserBundle:Login:login.html.twig',
             [
-                'error' => $exception ? $exception->getMessage() : null,
+                'error' => $this->getTranslatedErrorMessageFromAuthenticationException($exception),
             ]
+        );
+    }
+
+    /**
+     * @param AuthenticationException|null $exception
+     *
+     * @return null|string
+     */
+    private function getTranslatedErrorMessageFromAuthenticationException(AuthenticationException $exception = null)
+    {
+        if ($exception === null) {
+            return null;
+        }
+
+        switch (true) {
+            case $exception instanceof UsernameNotFoundException:
+                return $this->translateValidatorMessage('sumocoders.multiuserbundle.login.username_not_found');
+            case $exception instanceof BadCredentialsException:
+                return $this->translateValidatorMessage('sumocoders.multiuserbundle.login.bad_credentials');
+            case $exception instanceof UnsupportedUserException:
+                return $this->translateValidatorMessage('sumocoders.multiuserbundle.login.unsupported_user');
+            default:
+                return $this->translateValidatorMessage('sumocoders.multiuserbundle.something_went_wrong');
+        }
+    }
+
+    /**
+     * @param string $messageString
+     *
+     * @return string
+     */
+    private function translateValidatorMessage($messageString)
+    {
+        return $this->translator->trans(
+            $messageString,
+            [],
+            'validators'
         );
     }
 }
