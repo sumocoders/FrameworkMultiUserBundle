d871d9246f0b986b7a5396baede9793313a01538
Bruno Vitorino <bruno@sumocoders.be>
2017-06-20 11:12:04 +0200

    Create new email user provider.
    This user provider can be used to login users by using an email address.

---
+++ Security/ObjectUserEmailProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SumoCoders\FrameworkMultiUserBundle\Security;
+
+use SumoCoders\FrameworkMultiUserBundle\User\Interfaces\User;
+use SumoCoders\FrameworkMultiUserBundle\User\UserRepositoryCollection;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+
+class ObjectUserEmailProvider implements UserProviderInterface
+{
+    /** @var UserRepositoryCollection */
+    private $userRepositoryCollection;
+
+    /**
+     * @param UserRepositoryCollection $userRepositoryCollection
+     */
+    public function __construct(UserRepositoryCollection $userRepositoryCollection)
+    {
+        $this->userRepositoryCollection = $userRepositoryCollection;
+    }
+
+    public function loadUserByUsername($username)
+    {
+        foreach ($this->userRepositoryCollection->all() as $repository) {
+            $user = $repository->findByEmailAddress($username);
+
+            if ($user instanceof User) {
+                return $user;
+            }
+        }
+
+        throw new UsernameNotFoundException(
+            sprintf('Username "%s" does not exist.', $username)
+        );
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$this->supportsClass(get_class($user))) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass($class)
+    {
+        return $this->userRepositoryCollection->supportsClass($class);
+    }
+}
