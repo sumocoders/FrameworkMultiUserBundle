e49977d6fa3d33cbd1c1f2df4f80a9bfad3f3432
Bjorn Van Acker <bjorn@sumocoders.be>
2018-08-02 16:14:08 +0200

    Override constructor for repositories

--- User/DoctrineBaseUserRepository.php
+++ User/DoctrineBaseUserRepository.php
@@ -2,6 +2,13 @@
 
 namespace SumoCoders\FrameworkMultiUserBundle\User;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
+use SumoCoders\FrameworkMultiUserBundle\Entity\BaseUser;
+
 final class DoctrineBaseUserRepository extends AbstractUserRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, BaseUser::class);
+    }
 }
