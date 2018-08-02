e1d9b3099634eacab0e90bba211b07e3ac953791
Bjorn Van Acker <bjorn@sumocoders.be>
2018-08-02 16:27:47 +0200

    Use alias for autoloading base user repository

--- DependencyInjection/SumoCodersFrameworkMultiUserExtension.php
+++ DependencyInjection/SumoCodersFrameworkMultiUserExtension.php
@@ -28,6 +28,7 @@ class SumoCodersFrameworkMultiUserExtension extends Extension
         $loader->load('console.yml');
         $loader->load('controllers.yml');
         $loader->load('forms.yml');
+        $loader->load('repositories.yml');
         $loader->load('services.yml');
     }
 }
